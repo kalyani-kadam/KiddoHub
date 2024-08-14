c36c8161c973ba3c3b5cea07da2adf7fd7d3246e
kshitija-kurhade98 <kshitija.kurhade88@gmail.com>
2024-08-14 22:07:30 +0530

    admin panel changed.. registration link added

--- FrontEnd/kiddohub/src/Components/AdminComponent.js
+++ FrontEnd/kiddohub/src/Components/AdminComponent.js
@@ -1,43 +1,32 @@
 import React from "react";
-import '../Style/sidebar.css';
+import '../Style/sidebar.css'
 
 export default function AdminComponent(){
 
-    const handleAction = ()=>{
-        
-    }
     return(
         <div>
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
         
         {/* <!-- sidebar CSS--> */}
-        <link href="css/sidebar.css" rel="stylesheet" media="all"/>
+        <link href="Style/sidebar.css" rel="stylesheet" media="all"/>
         <body>
         <div class="sidebar">
             <a href="#home"><i class="fa fa-fw fa-home"></i> Home</a>
             <a href="#services"><i class="fa fa-fw fa-wrench"></i> Services</a>
-            <a href="/parenttable"><i class="fa fa-fw fa-user"></i> Clients</a>
+            <a href="#clients"><i class="fa fa-fw fa-user"></i> Clients</a>
             <a href="#contact"><i class="fa fa-fw fa-envelope"></i> Contact</a>
+            <a href="/parentregister"><i class="fa fa-fw fa-user"></i> Parent Registeration</a>
+            <a href="/childregister"><i class="fa fa-fw fa-child"></i> Child Registeration</a>
+            <a href="/doctorregister"><i class="fa fa-fw fa-user-md"></i> Doctor Registeration</a>
+            <a href="/staffregister"><i class="fa fa-fw fa-id-badge"></i> Staff Registeration</a>
+            <a href="/activityregister"><i class="fa fa-fw fa-calendar"></i> Activity Schedule</a>
+            
         </div>
         
-        <div className="admin-portal">
-      <h1>Admin Portal</h1>
-      {entities.map((entity) => (
-        <div key={entity} className="entity-section">
-          <h2>{entity} Management</h2>
-          <button onClick={() => handleAction('Add', entity)}>Add {entity}</button>
-          <button onClick={() => handleAction('Update', entity)}>Update {entity}</button>
-          <button onClick={() => handleAction('Delete', entity)}>Delete {entity}</button>
-          <button onClick={() => handleAction('View', entity)}>View {entity}</button>
-        </div>
-      ))}
-    </div>
+        
         </body>
-
-        
         </div>
     )
     
 }
-
